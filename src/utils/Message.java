edd0c9b0a352c12af499f50695271f3932f1181f
Troy Havelock <tshavelock@gmail.com>
2019-01-24 11:48:46 -0500

    Stubs for the message class

---
+++ src/utils/Message.java
@@ -0,0 +1,24 @@
+package utils;
+
+public class Message {
+
+    private String _message;
+
+    public Message(byte[] message) {
+
+    }
+
+    public Message(String message) {
+
+    }
+
+    /**
+     * Converts message into byte array
+     *
+     * @return byte array
+     */
+    public byte[] toByteArray() {
+
+        return null;
+    }
+}
