04541df179050bf409d4c9d6acccee294fbf695f
Troy Havelock <tshavelock@gmail.com>
2019-01-25 13:25:08 -0500

    Scheduler stub with port numbers

---
+++ src/main/Scheduler.java
@@ -0,0 +1,48 @@
+package main;
+
+public class Scheduler {
+    /* ===================================== */
+    /* ========== PRIVATE MEMBERS ========== */
+
+
+
+    /* ======================================= */
+    /* ========== PROTECTED MEMBERS ========== */
+
+
+
+    /* ==================================== */
+    /* ========== PUBLIC MEMBERS ========== */
+
+    public final int PORT_FLOOR     = 5000;
+    public final int PORT_ELEVATOR  = 5001;
+
+    /* ============================= */
+    /* ========== SETTERS ========== */
+
+
+
+    /* ============================= */
+    /* ========== GETTERS ========== */
+
+
+
+    /* ================================== */
+    /* ========== CONSTRUCTORS ========== */
+
+    /**
+     * Scheduler constructor
+     */
+    public Scheduler() {
+        // TODO: Implement
+    }
+
+    /* ============================= */
+    /* ========== METHODS ========== */
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
