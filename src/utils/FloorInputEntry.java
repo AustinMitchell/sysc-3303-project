57f363b19035efec7213e5e16e9ccaefcc869eee
Austin Mitchell <Austin Mitchell>
2019-01-26 14:33:41 -0500

    Adding direction enum, added more stuff to floor input entry

--- src/utils/FloorInputEntry.java
+++ src/utils/FloorInputEntry.java
@@ -1,5 +1,13 @@
 package utils;
 
 public class FloorInputEntry {
-    private TimeStamp _timestamp;
+    private TimeStamp 	_timestamp;
+    private int 		_floor;
+    private Direction 	_direction;
+    private int 		_car;
+    
+    public TimeStamp 	timestamp() { return _timestamp; }
+    public int 			floor()		{ return _floor; }
+    public Direction	direction() { return _direction; }
+    public int 			car()		{ return _car; }
 }
