6a1fb8e09ce79c87c9af823b3a913dbd8fa69d0f
Austin Mitchell <Austin Mitchell>
2019-01-26 19:06:01 -0500

    Adding basic functionality for using input files in floor object

--- src/main/Floor.java
+++ src/main/Floor.java
@@ -1,23 +1,37 @@
 package main;
 
-import java.net.DatagramSocket;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
-public class Floor { 
-    public static final int PORT = 5000;
+import network.ClientSocket;
+import utils.FloorInputEntry;
+import utils.ResLoader;
+
+public class Floor {
+    public static final String INPUT_FILE_PATH = "floor_input.in";
     
-    private DatagramSocket _sendSocket;      // Socket to send data on
-    private DatagramSocket _recvSocket;      // Socket to recieve data on
+    private ClientSocket _socket;
     
-    public Floor() {
-        _sendSocket = null;
-        _recvSocket = null;
-        
-        
+    public Floor() throws UnknownHostException, SocketException{
+        _socket = new ClientSocket("localhost", Scheduler.PORT_FLOOR);
     }
     
     public void run() {
-        
+        try {
+            BufferedReader inputFile = new BufferedReader(new InputStreamReader(ResLoader.load(INPUT_FILE_PATH)));
+            for (String line = inputFile.readLine(); line != null; line = inputFile.readLine()) {
+                System.out.println(new FloorInputEntry(line));
+            }
+        } catch (IOException e) {
+            System.err.println("Cannot read input file. ");
+            e.printStackTrace();
+            return;
+        }
+
     }
     
-    public static void main(String[] args) { new Floor().run(); }
+    public static void main(String[] args) throws UnknownHostException, SocketException { new Floor().run(); }
 }
