80c91707adcfa516360e3417b0458e7ced022980
Austin Mitchell <Austin Mitchell>
2019-01-26 19:04:05 -0500

    Adding toString override for direction enum

--- src/utils/Direction.java
+++ src/utils/Direction.java
@@ -2,5 +2,17 @@ package utils;
 
 public enum Direction {
     UP,
-    DOWN
+    DOWN;
+    
+    @Override
+    public String toString() {
+        switch(ordinal()) {
+        case 0:
+            return "UP";
+        case 1:
+            return "DOWN";
+        default:
+            return "invalid";
+        }
+    }
 }
