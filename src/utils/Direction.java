eed458cc671cc6d755c6e5bee50da4247d6cb356
AustinMitchell <n.austin.mitchell@gmail.com>
2019-01-29 23:39:02 -0500

    Added function to convert integers to Direction enum

--- src/utils/Direction.java
+++ src/utils/Direction.java
@@ -15,4 +15,15 @@ public enum Direction {
             return "invalid";
         }
     }
+    
+    public static Direction fromInt(int i) {
+        switch(i) {
+        case 0:
+            return UP;
+        case 1:
+            return DOWN;
+        default:
+            return null;
+        }
+    }
 }
