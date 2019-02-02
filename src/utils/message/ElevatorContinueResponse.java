14d8f531e415c660416420940975c5a86a615d1b
AustinMitchell <n.austin.mitchell@gmail.com>
2019-02-02 17:36:17 -0500

    Changing response to include floor number

--- src/utils/message/ElevatorContinueResponse.java
+++ src/utils/message/ElevatorContinueResponse.java
@@ -5,30 +5,36 @@ public class ElevatorContinueResponse {
     public static final MessageType MESSAGE_TYPE = MessageType.ELEVATOR_CONTINUE_RESPONSE;
     
     private byte _carID;
-    private boolean _response;
+    private byte _floor;
     
     /** Returns the car ID property */
     public int      carID()     { return _carID; }
     
-    /** Returns the continue response */
-    public boolean  response()  { return _response; }
+    /** Returns the continue response. -1 means continue, otherwise it will be the floor its stopping at */
+    public byte     response()  { return _floor; }
     
-    /** Creates a new request object from the given car ID */
-    public ElevatorContinueResponse(int carID, boolean response) {
-        _carID      = (byte)carID;
-        _response   = response;
+    /** 
+     * Creates a new request object from the given car ID and floor. If floor is -1, it means that the elevator should
+     * not continue.
+     * @param carID     ID of the car to send the message to
+     * @param floor     Floor to stop at, used to control the elevator lamps*/
+    public ElevatorContinueResponse(int carID, byte floor) {
+        _carID = (byte)carID;
+        _floor = floor;
     }
     
-    /** Creates a request object from the given byte array. First byte will be the message type, second byte is the car ID, third
-     * byte is the response */
+    /**
+     * Creates a request object from the given byte array. First byte will be the message type, second byte is the car ID, third
+     * byte is the floor to stop at, -1 mean do not stop.
+     * */
     public ElevatorContinueResponse(byte[] inputData) {
         MESSAGE_TYPE.verifyMessage(inputData);
-        _carID      = inputData[1];
-        _response   = (inputData[2] == 1);
+        _carID = inputData[1];
+        _floor = inputData[2];
     }
     
     /** Converts this object into a byte array. First byte is the message ID, second byte is the car ID, third byte is the response */
     public byte[] toBytes() {
-        return new byte[] { (byte)MESSAGE_TYPE.ordinal(), _carID, (byte)(_response ? 1 : 0) };
+        return new byte[] { (byte)MESSAGE_TYPE.ordinal(), _carID, _floor };
     }
 }
