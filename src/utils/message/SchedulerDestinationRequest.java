db8af48747f01f0db3a45639786740592d243056
AustinMitchell <n.austin.mitchell@gmail.com>
2019-02-02 17:28:19 -0500

    Changed destination request to allow multiple floors

--- src/utils/message/SchedulerDestinationRequest.java
+++ src/utils/message/SchedulerDestinationRequest.java
@@ -1,21 +1,30 @@
 package utils.message;
 
-/** Object for sending to an elevator car to determine which elevator button it needs to press */
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/** Object for sending to an elevator car to determine which elevator buttons it needs to press */
 public class SchedulerDestinationRequest {
-    public static final MessageType MESSAGE_TYPE = MessageType.ELEVATOR_CONTINUE_RESPONSE;
+    public static final MessageType MESSAGE_TYPE = MessageType.SCHEDULER_DESTINATION_REQUEST;
     
     private byte _carID;
-    private byte _destinationFloor;
+    private ArrayList<Byte> _destinationFloors;
     
     /** Returns the target car ID */
-    public int carID()              { return _carID; }
-    /** Returns the destination floor */
-    public int destinationFloor()   { return _destinationFloor; }
+    public int carID()                      { return _carID; }
+    /** Returns the destination floor at an index */
+    public int destinationFloor(int idx)    { return _destinationFloors.get(idx); }
+    /** Returns the number of destination floors */
+    public int destinationFloorCount()      { return _destinationFloors.size(); } 
+    
+    /** Add another destination floor to the request */
+    public void addFloor(int newFloor)      { _destinationFloors.add((byte)newFloor); }
 
     /** Creates a new request object from the given car ID and destination floor */
-    public SchedulerDestinationRequest(int carID, int destinationFloor) {
+    public SchedulerDestinationRequest(int carID) {
         _carID              = (byte)carID;
-        _destinationFloor   = (byte)destinationFloor;
+        _destinationFloors  = new ArrayList<>();
     }
     
     /** Creates a new request object from the byte array. First byte is the message type, second is the car ID, third
@@ -23,12 +32,24 @@ public class SchedulerDestinationRequest {
     public SchedulerDestinationRequest(byte[] inputData) {
         MESSAGE_TYPE.verifyMessage(inputData);
         _carID              = inputData[1];
-        _destinationFloor   = inputData[2];
+        _destinationFloors  = new ArrayList<>();
+        for (int i=0; i<inputData[2]; i++) {
+            addFloor(inputData[3+i]);
+        }
+    }
+    
+    private byte[] destinationsAsArray() {
+        byte[] listAsArray = new byte[_destinationFloors.size()];
+        for(int i=0; i<listAsArray.length; i++) {
+            listAsArray[i] = _destinationFloors.get(i);
+        }
+        return listAsArray;
     }
     
     /** Converts object into a byte array. First byte is the message type, second is the car ID, third is the target
      * destination floor */
     public byte[] toBytes() {
-        return new byte[] { (byte)MESSAGE_TYPE.ordinal(), _carID, _destinationFloor };
+        int bufferSize = 3+_destinationFloors.size();
+        return ByteBuffer.allocate(bufferSize).put((byte)MESSAGE_TYPE.ordinal()).put(_carID).put((byte)_destinationFloors.size()).put(destinationsAsArray()).array();
     }
 }
