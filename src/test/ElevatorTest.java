816d8f4a9f38ae9e85c1d1ad08a7351e31de0b0b
AustinMitchell <n.austin.mitchell@gmail.com>
2019-02-02 18:36:15 -0500

    Added one elevator test

--- src/test/ElevatorTest.java
+++ src/test/ElevatorTest.java
@@ -1,7 +1,30 @@
 package test;
 
+import java.util.Arrays;
+
+import main.Elevator;
+import main.ElevatorMotor;
+import utils.message.ElevatorActionResponse;
+import utils.message.ElevatorContinueRequest;
+
 public class ElevatorTest {
     public static void main(String[] args) {
+        Object o = new Object();
+        Elevator e = new Elevator(o, 3, 1);
         
+        new Thread(e).start();
+        
+        e.putMessage(new ElevatorActionResponse(1, ElevatorMotor.MotorState.UP).toBytes());
+        
+        synchronized(o) {
+            try {
+                o.wait();
+            } catch (InterruptedException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+        }
+        
+        System.out.println(Arrays.toString(new ElevatorContinueRequest(e.getMessage()).toBytes()));
     }
 }
