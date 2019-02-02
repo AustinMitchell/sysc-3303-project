bc1980af0c8b5fbba8350281de171a7175e6e578
Troy Havelock <tshavelock@gmail.com>
2019-02-01 21:27:24 -0500

    Floor subsystem now sends events in simulated real time

--- src/main/Floor.java
+++ src/main/Floor.java
@@ -5,7 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import network.socket.ClientSocket;
 import utils.ResLoader;
@@ -13,38 +17,85 @@ import utils.message.FloorInputEntry;
 
 public class Floor {
     public static final String INPUT_FILE_PATH = "floor_input.in";
-    
+
     private ClientSocket _schedulerSocket;
-    
+
     public Floor() throws UnknownHostException, SocketException{
         _schedulerSocket = new ClientSocket("localhost", Scheduler.PORT_FLOOR);
     }
-    
+
     public void run() throws IOException {
+        // Establish a connection with the Scheduler
         if (!_schedulerSocket.runSetupAndStartThreads()) {
             throw new RuntimeException("Something went wrong setting up socket; Aborting");
         }
-        
+
+        List<FloorInputEntry> entryList = new ArrayList<FloorInputEntry>();
+
+        // Read the input file and establish a stack of entries
         BufferedReader inputFile = new BufferedReader(new InputStreamReader(ResLoader.load(INPUT_FILE_PATH)));
         for (String line = inputFile.readLine(); line != null; line = inputFile.readLine()) {
             FloorInputEntry newEntry = new FloorInputEntry(line);
-            
-            System.out.println(String.format("Sending out new entry to Scheduler: %s", newEntry));            
-            
-            byte[] bytes = newEntry.toBytes();
-            System.out.println(String.format("Entry as bytes: %s", Arrays.toString(bytes)));
-            
-            _schedulerSocket.sendMessage(bytes);
-            _schedulerSocket.waitForMessage();
-            
-            FloorInputEntry returnMessage = new FloorInputEntry(_schedulerSocket.getMessage());
-            System.out.println(String.format("Recieved message from Scheduler: %s", returnMessage));
-            
+            entryList.add(newEntry);
         }
 
+        // Sort the list of entries to ensure that the times are in order and log it
+        Collections.sort(entryList);
+        printList(entryList);
+
+        // Send out the entries to the Scheduler, simulating the time, starting with the first entry
+        if (entryList.size() == 1) {
+            sendEntryToScheduler(entryList.get(0));
+        }
+        else {
+            for (int i = 0; i < entryList.size() - 1; i++) {
+                FloorInputEntry currentEntry = entryList.get(i);
+                FloorInputEntry nextEntry = entryList.get(i + 1);
+
+                int delay = nextEntry.differenceInMilliseconds(currentEntry);
+
+                sendEntryToScheduler(currentEntry);
+                try {
+                    TimeUnit.MILLISECONDS.sleep(delay);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            sendEntryToScheduler(entryList.get(entryList.size()-1));
+        }
     }
-    
+
+    /**
+     * Helper function used to print out the list of entries
+     *
+     * @param list
+     */
+    private void printList(List<FloorInputEntry> list) {
+        System.out.println("===== Floor entry list:");
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).toString());
+        }
+
+        System.out.println("");
+    }
+
+    private void sendEntryToScheduler(FloorInputEntry entry) {
+        System.out.println(String.format("Sending out new entry to Scheduler: %s", entry));
+
+        byte[] bytes = entry.toBytes();
+        System.out.println(String.format("Entry as bytes: %s", Arrays.toString(bytes)));
+
+        _schedulerSocket.sendMessage(bytes);
+        //_schedulerSocket.waitForMessage();
+
+        //FloorInputEntry returnMessage = new FloorInputEntry(_schedulerSocket.getMessage());
+        //System.out.println(String.format("Recieved message from Scheduler: %s", returnMessage));
+    }
+
     public static void main(String[] args) throws IOException {
         new Floor().run();
     }
 }
+
--- src/utils/message/FloorInputEntry.java
+++ src/utils/message/FloorInputEntry.java
@@ -4,12 +4,12 @@ import java.nio.ByteBuffer;
 
 public class FloorInputEntry implements Comparable<FloorInputEntry> {
     public static final MessageType MESSAGE_TYPE = MessageType.FLOOR_INPUT_ENTRY;
-    
+
     private TimeStamp   _timestamp;
     private byte        _floor;
     private Direction   _direction;
     private byte        _car;
-    
+
     /** Returns timestamp from entry */
     public TimeStamp    timestamp() { return _timestamp; }
     /** Returns floor selection from entry */
@@ -18,19 +18,19 @@ public class FloorInputEntry implements Comparable<FloorInputEntry> {
     public Direction    direction() { return _direction; }
     /** Returns car selection from entry */
     public int          car()       { return _car; }
-    
+
     /** Creates an entry from a line in the input file */
     public FloorInputEntry(String inputLine) {
         String[] splitLine = inputLine.split(" ");
-        
+
         if (splitLine.length != 4) {
             throw new RuntimeException("Invalid input: Did not have exactly 4 columns with single spaces between.");
         }
-        
+
         _timestamp = new TimeStamp(splitLine[0]);
-        
+
         _floor = (byte)Integer.parseInt(splitLine[1]);
-        
+
         switch(splitLine[2].toUpperCase()) {
         case "UP":
             _direction = Direction.UP;
@@ -41,10 +41,10 @@ public class FloorInputEntry implements Comparable<FloorInputEntry> {
         default:
             throw new RuntimeException("Invalid input in column 3: Did not match 'Up' or 'Down', case insensitive");
         }
-        
+
         _car = (byte)Integer.parseInt(splitLine[3]);
     }
-    
+
     /** Creates a new FloorInputEntry from a byte array. Only considers the first 9 bytes */
     public FloorInputEntry(byte[] inputData) {
         MESSAGE_TYPE.verifyMessage(inputData);
@@ -53,17 +53,28 @@ public class FloorInputEntry implements Comparable<FloorInputEntry> {
         _direction  = Direction.fromOrdinal(inputData[6]);
         _car        = inputData[7];
     }
-    
+
     @Override
     public String toString() {
         return String.format("Timestamp: %s, Floor: %d, Direction: %s, Car: %d", _timestamp.toString(), _floor, _direction, _car);
     }
-    
+
     /** Returns the entry as an array of bytes. First 5 bytes are the timestamp, then the next 3 are the floor, direction and car */
     public byte[] toBytes() {
-        return ByteBuffer.allocate(9).put((byte)MESSAGE_TYPE.ordinal()).put(_timestamp.toBytes()).put((byte)_floor).put((byte)_direction.ordinal()).put((byte)_car).array();
+        return ByteBuffer.allocate(9).put((byte)MESSAGE_TYPE.ordinal()).put(_timestamp.toBytes()).put(_floor).put((byte)_direction.ordinal()).put(_car).array();
     }
-    
+
+    /**
+     * Calculates the difference between self and another entry and returns the
+     * result in milliseconds
+     *
+     * @param entry
+     * @return difference in milliseconds
+     */
+    public int differenceInMilliseconds(FloorInputEntry entry) {
+        return this._timestamp.toMilliseconds() - entry.timestamp().toMilliseconds();
+    }
+
     /** Compares the timestamps of this entry and another entry. Earlier timestamps are considered "less than" later timestamps. */
     @Override
     public int compareTo(FloorInputEntry other) {
