3c1ae0963ca5b9ae91445f09c99dc66f0c81cf5e
Troy Havelock <tshavelock@gmail.com>
2019-02-16 15:49:43 -0500

    More on the JUnit test

--- src/unittest/ElevatorTest.java
+++ src/unittest/ElevatorTest.java
@@ -2,9 +2,7 @@ package unittest;
 
 import static org.junit.Assert.*;
 
-import java.sql.Date;
-import java.util.Timer;
-
+import java.util.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -66,7 +64,38 @@ public class ElevatorTest {
         assertEquals(MotorState.STATIONARY, continueRequest.actionTaken());
 
         // ===== Testing ElevatorContinueResponse =====
+        // Test when the car keeps going
         elevator.putMessage(continueResponse1.toBytes());
+
+        response = elevator.getMessage();
+
+        assertNull(response);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        response = elevator.getMessage();
+
+        assertEquals(MessageType.ELEVATOR_CONTINUE_REQUEST.ordinal(), response[0]);
+        assertEquals(testCarID, response[1]);
+        assertEquals(MotorState.UP, elevator.motorState());
+
+        // Test when the car needs to stop
+        elevator.putMessage(continueResponse2.toBytes());
+
+        response = elevator.getMessage();
+        assertNull(response);
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(MotorState.STATIONARY, elevator.motorState());
     }
 
 }
